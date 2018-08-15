71c57176ccfe1336c7bbf19cf6c0df00af4bd72c
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-08-16 00:08:24 +0200

    Api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get '(*endpoint)', controller: :api, action: :route_not_found
+    end
+  end
+
   root 'application#index'
   get '(*frontend)', to: 'application#index'
 end
