40a70c300c781b790e4bef129f21359ba3d9c44a
j8seangel <jseangel@gmail.com>
2018-08-17 12:25:51 +0200

    Put total-ghg below home folder

--- app/javascript/app/pages/home/home-component.jsx
+++ app/javascript/app/pages/home/home-component.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Section } from 'cw-components';
 import background from 'assets/hero';
-import TotalGHGEmissions from 'components/total-ghg-emissions';
+import TotalGHGEmissions from 'components/home/total-ghg-emissions';
 import FlagshipProgrammes from 'components/home/flagship-programmes';
 import Cards from 'components/home/cards';
 import styles from './home-styles.scss';
