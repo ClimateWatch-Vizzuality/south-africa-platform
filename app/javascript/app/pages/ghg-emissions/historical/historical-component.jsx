1909effe2be0f26c6a125575c034c13f1768719b
j8seangel <jseangel@gmail.com>
2018-08-22 11:22:14 +0200

    Historical emissions section

--- app/javascript/app/pages/ghg-emissions/historical/historical-component.jsx
+++ app/javascript/app/pages/ghg-emissions/historical/historical-component.jsx
@@ -1,17 +1,152 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import styles from './historical-styles.scss';
+import ModalMetadata from 'components/modal-metadata';
+import SectionTitle from 'components/section-title';
+import { TabletLandscape, TabletPortraitOnly } from 'components/responsive';
+import {
+  Section,
+  ButtonGroup,
+  Button,
+  Icon,
+  Dropdown,
+  Chart
+} from 'cw-components';
+import MetadataProvider from 'providers/metadata-provider';
+import GHGEmissionsProvider from 'providers/ghg-emissions-provider';
+import WorldBankProvider from 'providers/world-bank-provider';
+import iconInfo from 'assets/icons/info';
+import downloadIcon from 'assets/icons/download';
 
-class Nav extends PureComponent {
+import styles from './historical-styles';
+
+class GHGHistoricalEmissions extends PureComponent {
+  handleSectorChange = ({ value }) => {
+    const { onFilterChange } = this.props;
+    onFilterChange({ sector: value });
+  };
+
+  handleMetricChange = ({ value }) => {
+    const { onFilterChange } = this.props;
+    onFilterChange({ metric: value });
+  };
+
+  handleInfoClick = () => {
+    this.props.setModalMetadata({
+      slugs: 'historical_emissions_cait',
+      open: true
+    });
+  };
+
+  handleDownloadClick = () => {
+    console.info('TODO: link todownload data endpoint', this.props);
+  };
+
   render() {
+    const {
+      sectorSelected,
+      sectorOptions,
+      metricSelected,
+      metricOptions,
+      emissionsParams,
+      chartData
+    } = this.props;
+
+    const dropdowns = (
+      <div className={styles.dropdowWrapper}>
+        <Dropdown
+          label="Sector"
+          theme={{ wrapper: styles.dropdown }}
+          options={sectorOptions}
+          value={sectorSelected}
+          onValueChange={this.handleSectorChange}
+          hideResetButton
+        />
+        <Dropdown
+          label="Metric"
+          theme={{ wrapper: styles.dropdown }}
+          options={metricOptions}
+          value={metricSelected}
+          onValueChange={this.handleMetricChange}
+          hideResetButton
+        />
+      </div>
+    );
+    const toolbar = (
+      <div className={styles.toolbarButtons}>
+        <ButtonGroup theme={{ wrapper: styles.buttonWrapper }}>
+          <Button
+            onClick={this.handleInfoClick}
+            theme={{ button: styles.infobutton }}
+          >
+            <Icon icon={iconInfo} />
+          </Button>
+          <Button
+            onClick={this.handleDownloadClick}
+            theme={{ button: styles.infobutton }}
+          >
+            <Icon icon={downloadIcon} />
+          </Button>
+        </ButtonGroup>
+      </div>
+    );
     return (
-     <p className={styles.text}>The historical ghg emissions section</p>
+      <React.Fragment>
+        <Section theme={styles}>
+          <SectionTitle title="Historical emissions" />
+          <TabletLandscape>
+            {matches => {
+              if (matches) {
+                return (
+                  <div className={styles.toolbar}>
+                    {dropdowns}
+                    {toolbar}
+                  </div>
+                );
+              }
+              return dropdowns;
+            }}
+          </TabletLandscape>
+          <div className={styles.chart}>
+            <Chart
+              type="line"
+              dots={false}
+              customMessage="Emissions data not available"
+              hideRemoveOptions
+              {...chartData}
+            />
+          </div>
+          <TabletPortraitOnly>
+            {toolbar}
+          </TabletPortraitOnly>
+        </Section>
+        <MetadataProvider meta={[ 'sectors', 'ghg' ]} />
+        {emissionsParams && <GHGEmissionsProvider params={emissionsParams} />}
+        <WorldBankProvider />
+        <ModalMetadata />
+      </React.Fragment>
     );
   }
 }
 
-Nav.propTypes = {};
+GHGHistoricalEmissions.propTypes = {
+  chartData: PropTypes.object,
+  sectorOptions: PropTypes.array,
+  sectorSelected: PropTypes.object,
+  metricOptions: PropTypes.array,
+  metricSelected: PropTypes.object,
+  emissionsParams: PropTypes.object,
+  onFilterChange: PropTypes.func.isRequired,
+  setModalMetadata: PropTypes.func.isRequired,
+  updateFiltersSelected: PropTypes.func.isRequired
+};
 
-Nav.defaultProps = {};
+GHGHistoricalEmissions.defaultProps = {
+  chartData: {},
+  sectorOptions: [],
+  sectorSelected: null,
+  metricOptions: [],
+  metricSelected: null,
+  emissionsParams: null
+};
 
-export default Nav;
+export default GHGHistoricalEmissions;
