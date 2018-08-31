0fc93cbf520b192bec636337e078d343f386c8b7
Javi Abia <javiabia@eriks-MacBook-Air.local>
2018-08-31 17:01:33 +0200

    Create bubbleChart component

--- app/javascript/app/components/bubble-chart/bubble-chart-component.jsx
+++ app/javascript/app/components/bubble-chart/bubble-chart-component.jsx
@@ -1,5 +1,56 @@
-import React from 'react';
+import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import ReactTooltip from 'react-tooltip';
+import { chartDataCalculation } from './bubble-chart-d3-calculation';
+import styles from './bubble-chart-styles';
 
-const BubbleChart = () => <div>BUBBLE CHART</div>;
+class BubbleChart extends PureComponent {
+  componentDidUpdate() {
+    setTimeout(
+      () => {
+        ReactTooltip.rebuild();
+      },
+      100
+    );
+  }
+
+  render() {
+    const { width, height, handleNodeClick, data } = this.props;
+    const charData = data && chartDataCalculation(width, data);
+    return (
+      <Fragment>
+        <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+          {
+            charData && charData.children.map(d => (
+              <g
+                key={d.value}
+                transform={`translate(${d.x},${d.y})`}
+                onClick={e => handleNodeClick(e, d.data.id)}
+              >
+                <circle
+                  r={d.r}
+                  data-for="chartTooltip"
+                  data-tip={`${d.data.value} ${d.data.unit}`}
+                  fill={d.data.color}
+                  className={styles.circle}
+                />
+              </g>
+              ))
+          }
+        </svg>
+        <ReactTooltip place="left" id="chartTooltip" className="chartTooltip" />
+      </Fragment>
+    );
+  }
+}
+
+BubbleChart.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  handleNodeClick: PropTypes.func.isRequired,
+  data: PropTypes.array
+};
+
+BubbleChart.defaultProps = { data: [] };
 
 export default BubbleChart;
