1180175fcfe4e1210f2eba58a6ed872c946ede48
Alvaro Leal <pursuance@gmail.com>
2018-09-11 12:45:51 +0200

    Remove cards and fix paddings in main text

--- app/javascript/app/pages/home/home-component.jsx
+++ app/javascript/app/pages/home/home-component.jsx
@@ -3,7 +3,6 @@ import { Section } from 'cw-components';
 import background from 'assets/hero';
 import TotalGHGEmissions from 'components/home/total-ghg-emissions';
 import FlagshipProgrammes from 'components/home/flagship-programmes';
-import Cards from 'components/home/cards';
 import styles from './home-styles.scss';
 
 class Home extends PureComponent {
@@ -17,9 +16,6 @@ class Home extends PureComponent {
                 The South African Climate Report captures South Africa’s response to climate change with emphasis on climate change mitigation, and offers open data, visualizations and analysis to help policy-makers, researchers, investors and the general public gather insights on the country’s climate progress.
               </p>
             </div>
-          </div>
-          <div className="layout-container">
-            <Cards />
           </div>
         </Section>
         <TotalGHGEmissions />
