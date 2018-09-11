8057b45a741a5de47850cc2f8cfe43f667e7ffe3
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-09-11 13:35:49 +0100

    Inventory improvement projects specs

---
+++ spec/controllers/api/v1/inventory_improvement_projects_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe Api::V1::InventoryImprovementProjectsController, type: :controller do
+  context do
+    let!(:some_projects) {
+      FactoryBot.create_list(:inventory_improvement_project, 3)
+    }
+
+    describe 'GET index' do
+      it 'returns a successful 200 response' do
+        get :index, format: :json
+        expect(response).to be_success
+      end
+
+      it 'lists all inventory improvement projects' do
+        get :index, format: :json
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body.length).to eq(3)
+      end
+
+      it 'responds to csv' do
+        get :index, format: :csv
+        expect(response.content_type).to eq('text/csv')
+        expect(response.headers['Content-Disposition']).
+          to eq('attachment; filename="inventory_improvement_projects.csv"')
+      end
+    end
+  end
+end
