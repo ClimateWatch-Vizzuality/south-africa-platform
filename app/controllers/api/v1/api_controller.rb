2170bc61aee61a9fb6950c96eda61ae43a7f1ea4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-09-13 11:59:08 +0100

    Removed unnecessary code

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -5,30 +5,6 @@ module Api
       include ::ClimateWatchEngine::Cors
       include ::ClimateWatchEngine::ExceptionResponses
       include ::ClimateWatchEngine::Caching
-
-      rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
-
-      def resource_not_found
-        render json: {
-          code: 404,
-          status: 'resource not found'
-        }, status: :not_found
-      end
-
-      def route_not_found
-        render json: {
-          code: 404,
-          status: "#{request.params[:endpoint]} not found"
-        }, status: :not_found
-      end
-
-      before_action :set_access_control_headers
-
-      def set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = ENV['CORS_WHITELIST']
-        headers['Access-Control-Allow-Methods'] = 'GET'
-        headers['Access-Control-Expose-Headers'] = 'Link, Total, Per-Page'
-      end
     end
   end
 end
