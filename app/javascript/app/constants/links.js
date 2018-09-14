99cbfb9ad235578358e42c9f3ec28411b5eb1e1f
Alvaro Leal <pursuance@gmail.com>
2018-09-14 17:57:33 +0200

    Remove extra default export

--- app/javascript/app/constants/links.js
+++ app/javascript/app/constants/links.js
@@ -1,3 +1,2 @@
 export const ENVIRONMENTAL_AFFAIRS_URL = 'https://www.environment.gov.za/';
 export const GHG_NATIONAL_REPORT = 'https://unfccc.int/sites/default/files/resource/28147395_South%20Africa-BUR2-1-South%20Africa_%20GHG%20Inventory%20Report%20-%202000-2012.pdf';
-export default { ENVIRONMENTAL_AFFAIRS_URL };
