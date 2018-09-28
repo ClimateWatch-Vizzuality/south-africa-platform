c470150da62721a4f52ef450938987c33013fbe5
Tiago Santos <santos.tiago@gmail.com>
2018-09-28 11:31:13 +0100

    Added to the Flagship Programme and Theme serializer some fields that were left out

--- app/serializers/api/v1/mitigation/flagship_programme_serializer.rb
+++ app/serializers/api/v1/mitigation/flagship_programme_serializer.rb
@@ -2,20 +2,23 @@
 #
 # Table name: flagship_programmes
 #
-#  id                     :bigint(8)        not null, primary key
-#  mitigation_category_id :integer
-#  title                  :string
-#  description            :text
-#  position               :integer
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
+#  id                :bigint(8)        not null, primary key
+#  sub_programs      :text
+#  description       :text
+#  position          :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  work_package      :text
+#  outcomes          :text
+#  flagship_theme_id :integer
 #
 
 module Api
   module V1
     module Mitigation
       class FlagshipProgrammeSerializer < ApplicationSerializer
-        attributes :description, :position
+        attributes :id, :description, :position, :sub_programs, :work_package,
+                   :outcomes
 
         belongs_to :flagship_theme,
                    serializer: Api::V1::Mitigation::FlagshipThemeSerializer
