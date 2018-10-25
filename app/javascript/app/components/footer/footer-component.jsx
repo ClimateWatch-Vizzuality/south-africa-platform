1b1d4af1d3fe99ea88ba87cd21d2bd489c753b95
Alvaro Leal <pursuance@gmail.com>
2018-10-25 14:19:04 +0200

    Add logos

--- app/javascript/app/components/footer/footer-component.jsx
+++ app/javascript/app/components/footer/footer-component.jsx
@@ -4,6 +4,7 @@ import DownloadBox from 'components/download-box';
 import cx from 'classnames';
 
 import partners from 'constants/partners';
+import founders from 'constants/founders';
 import styles from './footer-styles.scss';
 
 class Footer extends PureComponent {
@@ -11,29 +12,67 @@ class Footer extends PureComponent {
     return (
       <footer className={styles.footer}>
         <div className={styles.grid}>
-          <div className={styles.content}>
-            <span>In cooperation with:</span>
-            {partners.map(
-              partner => partner.img && (
-              <div key={partner.img.alt} className={styles.logoContainer}>
-                <a
-                  className={cx(
-                        styles.logo,
-                        styles[partner.img.customClass]
-                      )}
-                  href={partner.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    className={styles.defaultLogo}
-                    src={partner.img.src}
-                    alt={partner.img.alt}
-                  />
-                </a>
+          <div>
+            <div className={styles.content}>
+              <div className={styles.section}>
+                <span>In cooperation with:</span>
+                {partners.map(
+                  partner => partner.img && (
+                  <div>
+                    <div
+                      key={partner.img.alt}
+                      className={styles.logoContainer}
+                    >
+                      <a
+                        className={cx(
+                              styles.logo,
+                              styles[partner.img.customClass]
+                            )}
+                        href={partner.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          className={styles.defaultLogo}
+                          src={partner.img.src}
+                          alt={partner.img.alt}
+                        />
+                      </a>
+                    </div>
+                  </div>
+                    )
+                )}
               </div>
-                )
-            )}
+              <div className={styles.section}>
+                <span>
+                  Generous funding for this initiative is provided by:
+                </span>
+                <div className={styles.founders}>
+                  {founders.map(founder => (
+                    <div
+                      key={founder.img.alt}
+                      className={styles.foundersContainer}
+                    >
+                      <a
+                        className={cx(
+                          styles.logo,
+                          styles[founder.img.customClass]
+                        )}
+                        href={founder.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          className={styles.foundersLogo}
+                          src={founder.img.src}
+                          alt={founder.img.alt}
+                        />
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
             <div className={styles.disclaimer}>
               Copyright &#169; Department of Environmental Affairs{' '}
               {new Date().getFullYear()}
