4c557498a4af067aaadccb08ab4b7b01f64c20fb
Alvaro Leal <alvaro.leal@vizzuality.com>
2018-10-30 20:00:36 +0100

    Add sustainable benefits table

--- app/javascript/app/pages/mitigation/mitigation-effects/mitigation-effects-component.jsx
+++ app/javascript/app/pages/mitigation/mitigation-effects/mitigation-effects-component.jsx
@@ -2,33 +2,42 @@ import React, { PureComponent } from 'react';
 import MitigationEffectsProvider from 'providers/mitigation-effects-provider';
 import SectionTitle from 'components/section-title';
 import TabSwitcher from 'components/tab-switcher';
-import { Loading } from 'cw-components';
+import DataTable from 'components/data-table';
 import ModalInfo from 'components/modal-info';
+import { PropTypes } from 'prop-types';
 import Summary from './summary';
-
 import styles from './mitigation-effects-styles';
 
 const SUMMARY_KEY = 'summary';
 const SUSTAINABLE_DEVELOPMENT_KEY = 'sustainable-development';
 
 class MitigationEffects extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tabs: [
-        { name: 'SUMMARY', value: SUMMARY_KEY, component: <Summary /> },
-        {
-          name: 'SUSTAINABLE DEVELOPMENT BENEFITS',
-          value: SUSTAINABLE_DEVELOPMENT_KEY,
-          component: <Loading height="660" />,
-          disabled: true
-        }
-      ]
-    };
+  handleTabChange = ({ value }) => {
+    const { updateQueryParam, section } = this.props;
+    updateQueryParam({ query: { tab: value }, section });
+  };
+
+  renderTabs() {
+    const { tableData } = this.props;
+    return [
+      { name: 'SUMMARY', value: SUMMARY_KEY, component: <Summary /> },
+      {
+        name: 'SUSTAINABLE DEVELOPMENT BENEFITS',
+        value: SUSTAINABLE_DEVELOPMENT_KEY,
+        component: tableData &&
+          (
+            <DataTable
+              tableData={tableData}
+              dynamicRowsHeight
+              setColumnWidth={() => 150}
+            />
+          )
+      }
+    ];
   }
 
   render() {
-    const { tabs } = this.state;
+    const { activeTabValue } = this.props;
     return (
       <div className={styles.row}>
         <SectionTitle isSubtitle title="Mitigation Effects" infoButton />
@@ -36,11 +45,11 @@ class MitigationEffects extends PureComponent {
           Through the National Climate Change Response Monitoring and Evaluation System (M&E System), South Africa is continuously striving to quantify the effects of mitigation policies, strategies and actions. This section presents the mitigation actions for which mitigation effects and sustainable development co-benefits have been quantified.
         </ModalInfo>
         <TabSwitcher
-          tabs={tabs}
+          tabs={this.renderTabs()}
           searchActive={false}
           actionsActive={false}
-          onTabChange={() => {
-          }}
+          activeTabValue={activeTabValue}
+          onTabChange={this.handleTabChange}
         />
         <MitigationEffectsProvider />
       </div>
@@ -48,6 +57,17 @@ class MitigationEffects extends PureComponent {
   }
 }
 
-MitigationEffects.propTypes = {};
+MitigationEffects.propTypes = {
+  tableData: PropTypes.object,
+  updateQueryParam: PropTypes.func.isRequired,
+  section: PropTypes.string,
+  activeTabValue: PropTypes.string
+};
+
+MitigationEffects.defaultProps = {
+  tableData: {},
+  activeTabValue: 'summary',
+  section: 'mitigation-effects'
+};
 
 export default MitigationEffects;
--- app/javascript/app/pages/mitigation/mitigation-effects/mitigation-effects.js
+++ app/javascript/app/pages/mitigation/mitigation-effects/mitigation-effects.js
@@ -1,3 +1,7 @@
+import { connect } from 'react-redux';
 import Component from './mitigation-effects-component';
 
-export default Component;
+import { getSustainableDevelopmentData } from './mitigation-effects-selectors';
+import * as actions from './mitigation-effects-actions';
+
+export default connect(getSustainableDevelopmentData, actions)(Component);
