11b3192829756bf21978b54d226bb2e71ecef758
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2018-11-07 15:46:21 +0000

    Show all sections on index page

--- app/admin/content_management.rb
+++ app/admin/content_management.rb
@@ -3,24 +3,11 @@ ActiveAdmin.register SectionContent do
   config.filters = false
   permit_params :title, :description
 
-  controller do
-    def scoped_collection
-      SectionContent.where(subsection_id: nil)
-    end
-  end
-
   form do |f|
     f.inputs 'Main Section' do
       f.input :name, input_html: {readonly: true}
       f.input :title, label: 'Title'
       f.input :description, label: 'Description'
-    end
-    f.inputs 'Subsections' do
-      f.has_many :subsections, new_record: false, allow_destroy: false do |a|
-        a.input :name, input_html: {readonly: true}
-        a.input :title
-        a.input :description
-      end
     end
     f.actions
   end
@@ -39,11 +26,13 @@ ActiveAdmin.register SectionContent do
       column :title
       column :description
     end
-    h3 'Subsections'
-    table_for section_content.subsections do
-      column :name
-      column :title
-      column :description
+    unless section_content.subsections.empty?
+      h3 'Subsections'
+      table_for section_content.subsections do
+        column :name
+        column :title
+        column :description
+      end
     end
   end
 end
