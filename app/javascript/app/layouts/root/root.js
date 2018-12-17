7bdc378e4c498a2f088403dc324936e1e8c73419
Simao Belchior <simaobelchior@gmail.com>
2018-12-17 12:24:14 +0000

    pass location to Root component

--- app/javascript/app/layouts/root/root.js
+++ app/javascript/app/layouts/root/root.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import Component from './root-component';
 
 const mapStateToProps = ({ location }) => ({
-  route: location.routesMap[location.type]
+  route: location.routesMap[location.type],
+  location: location
 });
 
 export default connect(mapStateToProps, null)(Component);
